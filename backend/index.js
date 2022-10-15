de9dac5a782fc143416d0ad52666d12fa6a408b9
Kanade <16192291+andywang0625@users.noreply.github.com>
2022-10-15 18:23:19 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,11 @@ function getFileExtension(fileName){
     fileExtension = fileName.replace(/^.*\./, '');
     return fileExtension;
 }
+
+function getFileNameWithoutExtension(filename){
+    return filename.substr(0, filename.lastIndexOf("."))
+}
+
 function isImage(fileName){
     var fileExt = getFileExtension(fileName);
     var imagesExtension = ["png", "jpg", "jpeg"];
@@ -33,6 +38,7 @@ app.use('/img', express.static(configs.image_folder));
 const getRandomImage = (callback) => {
     return randomFile(configs.image_folder, (err, file) => {
         if(err) return err;
+        if (file === undefined) return callback();
         if (isImage(file)) return callback(file);
         return getRandomImage(callback);
     });
@@ -40,6 +46,7 @@ const getRandomImage = (callback) => {
 
 app.get('/get_image', (req, res) => {
     getRandomImage((img) => {
+        if (img === undefined) return res.sendStatus(404);
         console.log(`Got image ${img}`);
         res.json({filename: img});
     })
@@ -55,8 +62,14 @@ app.put('/set_image', (req, res) => {
     fs.rename(`${configs.image_folder}/${imgName}`, `${configs.image_folder}/${imgClass}/${imgName}`, (err) => {
         if (err && err.code == "ENOENT") {
             fs.mkdirSync(`${configs.image_folder}/${imgClass}`);
+            fs.renameSync(`${configs.image_folder}/${imgName}`, `${configs.image_folder}/${imgClass}/${imgName}`);
         } else if (err) {
             throw err;
+        }
+        const txtFileName = `${getFileNameWithoutExtension(imgName)}.txt`
+        console.log(txtFileName);
+        if (fs.existsSync(`${configs.image_folder}/${txtFileName}`)) {
+            fs.renameSync(`${configs.image_folder}/${txtFileName}`, `${configs.image_folder}/${imgClass}/${txtFileName}`)
         }
         console.log(`${configs.image_folder}/${imgName} was copied to ${configs.image_folder}/${imgClass}/${imgName}`);
         return res.sendStatus(202)
